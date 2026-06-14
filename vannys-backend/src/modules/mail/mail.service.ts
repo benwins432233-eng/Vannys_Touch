5725672aafc3121f7714659c27bcbb1759b85c59
benwins432233-eng <benwins432233@gmail.com>
2026-06-14 03:22:53 +0100

    Installation de resend

--- vannys-backend/src/modules/mail/mail.service.ts
+++ vannys-backend/src/modules/mail/mail.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Resend } from '@resend/node';
+import { Resend } from 'resend';
 import { Order, User } from '@prisma/client';
 
 type OrderWithItems = Order & {
